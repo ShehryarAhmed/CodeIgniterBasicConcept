d545a56478617b138570c5fefeff30941d357729
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-10-08 23:47:44 +0500

    insert success full data in table and also working configr $autoload['libraries'] = array('database'); and also working in database.php to set db connection

--- CodeIgniter-3.1.6/application/controllers/test.php
+++ CodeIgniter-3.1.6/application/controllers/test.php
@@ -4,7 +4,13 @@ class Test extends CI_Controller{
      public function index(){
         echo "Hello Test php";
         $this->load->view('test');
-    }
+    echo "Model";
+   $this->load->model('Model_class');
+
+   $data = array('name'=>'Shehryar');
+   
+   $this->db->insert("student",$data); 
+}
       
 }
 ?>
---
+++ CodeIgniter-3.1.6/application/models/Model_class.php
@@ -0,0 +1,8 @@
+<?php 
+   Class Model_class extends CI_Model { 
+	
+      Public function __construct() { 
+         parent::__construct(); 
+      } 
+   } 
+?>
