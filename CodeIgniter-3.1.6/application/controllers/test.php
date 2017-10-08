123f7bae5610a6cd5875a92d71c5a6adf8ccf2ce
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-10-08 18:20:59 +0500

    working on view

--- CodeIgniter-3.1.6/application/controllers/test.php
+++ CodeIgniter-3.1.6/application/controllers/test.php
@@ -1,16 +1,10 @@
 <?php 
 class Test extends CI_Controller{
-     public function hello()
-     {
-         echo "hello php";
-     }
-     
+
      public function index(){
         echo "Hello Test php";
+        $this->load->view('test');
     }
       
-    public function Test(){
-        echo "constructer";
-    }
 }
 ?>
---
+++ CodeIgniter-3.1.6/application/views/test.php
@@ -0,0 +1,6 @@
+<html>
+    <title>Code Igniter</title>
+<body>
+   <h1> Code Igniter View </h1>
+</body>
+    </html>
