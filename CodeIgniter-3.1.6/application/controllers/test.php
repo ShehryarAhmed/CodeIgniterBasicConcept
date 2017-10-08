d5b59b82e762f0deb4c55f44afd59700c7302772
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-10-08 17:40:31 +0500

    working on controller class

---
+++ CodeIgniter-3.1.6/application/controllers/test.php
@@ -0,0 +1,16 @@
+<?php 
+class Test extends CI_Controller{
+     public function hello()
+     {
+         echo "hello php";
+     }
+     
+     public function index(){
+        echo "Hello Test php";
+    }
+      
+    public function Test(){
+        echo "constructer";
+    }
+}
+?>
